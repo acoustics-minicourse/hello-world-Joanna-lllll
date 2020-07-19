44dc5a6b2185fc49b4fc9044541988697d96e430
Joanna-lllll <68472609+Joanna-lllll@users.noreply.github.com>
2020-07-19 03:27:27 +0000

    Update hello-world.py
    Thanks for giving this course.

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,8 @@
 ### Write your code here
+pip install numpy
+pip install matplotlib.pyplot
+import numpy
+import matplotlib.pyplot
+print ("Jieqiong")
+python hello-world.py
+git add ./hello-world.py
